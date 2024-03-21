962ec2b1111901fb62b363619ad4013a8b13d795
kimhyerim01 <khrim981209@gmail.com>
2024-03-21 22:13:55 +0900

    Add Py File [pythontest1]

---
+++ pythontest1.py
@@ -0,0 +1,15 @@
+def get_radius(prompt):
+    r = int(input(prompt))
+    return r
+
+def get_circle_area(rr):
+    rrr = 3.14 * rr * rr
+    return rrr
+
+
+print('시작')
+rr = get_radius('넓이를 구하고자 하는 원의 반지름은?')
+result = get_circle_area(rr)
+print('반지름', rr, '인 원의 넓이 = 3.14 x ',rr, 'x ',rr, '=' , result)
+print('마침')
+
