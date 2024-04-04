18c1fa457415b101e8eb8e8b6bbaff19a7a0a11d
kimhyerim01 <163461154+kimhyerim01@users.noreply.github.com>
2024-04-04 23:13:27 +0900

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,20 @@
+import sale as eSale
+
+sale2 = int(input('할인율은?'))
+r = won * 100/(100-sale2)
+
+print('A상품의 할인된 가격은?', won)
+print('B상품의 할인된 가격은?', won)
+    
+def get_fixed_priced(r) :
+    print('A상품의 정가는', r, '원')
+    print('B상품의 정가는', r , '원')
+    return sale
+
+
+
+
+    
+    
+    
+    
