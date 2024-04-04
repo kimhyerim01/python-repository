4f92a1e918b77bfe0562f690617fbaab6a209002
kimhyerim01 <163461154+kimhyerim01@users.noreply.github.com>
2024-04-04 23:58:34 +0900

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,11 +1,11 @@
 import get_fixed_priced as fPrice
 
 sale_rate = int(input('할인률은?'))
-print('할인율은?',sale_rate)
+('할인율은?',sale_rate)
 A_price = int(input('A 상품의 할인된 가격은?'))
-print('A 상품의 할인된 가격은?', A_price)
+('A 상품의 할인된 가격은?', A_price)
 B_price = int(input('B 상품의 할인된 가격은?'))
-print('B 상품의 할인된 가격은?', B_price)
+('B 상품의 할인된 가격은?', B_price)
 
 print('A 상품의 정가는?', fPrice.get_fixed_priced(sale_rate, A_price), '원')
 print('B 상품의 정가는?', fPrice.get_fixed_priced(sale_rate, B_price), '원')
